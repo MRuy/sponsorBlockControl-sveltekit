47c1ec99c57df72b8f0d51c87f56f1cd8804869f
MRuy <nanobyte@ymail.com>
2022-05-26 01:51:58 +0000

    Deploying to gh-pages from  @ 620dd93b7d9db4a58c6302b66b7613640778d038 🚀

--- _app/chunks/config-8bce724e.js
+++
@@ -1,1 +0,0 @@
-const i=["sponsor","selfpromo","interaction","intro","outro","preview","music_offtopic","poi_highlight","filler"],o=["Sponsor","Unpaid/Self Promotion","Interaction Reminder (Subscribe)","Intermission/Intro Animation","Endcards/Credits","Preview/Recap","Music: Non-Music Section","Highlight","Filler Tangent"];export{o as a,i as c};
